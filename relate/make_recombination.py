d291b31b1de5e6fba9438abc210bdc2e0fe4426a
doublesquare88 <158044728+doublesquare88@users.noreply.github.com>
2024-08-07 00:49:10 +0200

    Update make_recombination.py
    #simple script to make Relate map recombination file from LDhelmet output, but still needs some manual editing: 
    #e.g. for scaffold12 of Trichaptum:
    #cat output_Circumboreal_NorAm_NB_scaffold12_LDhat_lk_post_to_text.txt | grep -v "#\|a"  > output_Circumboreal_NorAm_NB_scaffold12_LDhat_lk_post_to_text_no_header.txt
    #get CM by multiplying second column with 100/(4*N_e) which is 100/(4*100000)= 0.00025
    #cat output_Circumboreal_NorAm_NB_scaffold12_LDhat_lk_post_to_text_no_header.txt | awk '{FS=" "}{print $1}' > testc1
    #cat output_Circumboreal_NorAm_NB_scaffold12_LDhat_lk_post_to_text_no_header.txt | awk '{FS=" "}{print $3}' > testc2
    #cat testc2 | awk '{print $1*0.00025}' > testc2_r
    #cat testc2_r | awk '{print $1/2.381207}' > testc2_r_mb #(length of scaffold12 is 2381207)
    #python make_recombination.py testc1_c2_r_mb testc1c2c3
    #edit in last line: 2346732 0	        7.6464006440790200
    #edit in headers: Position.bp. Rate.cM.Mb. Map.cM.
    #convert from tab to space delimitation

--- relate/make_recombination.py
+++ relate/make_recombination.py
@@ -1,4 +1,18 @@
 #! ~/miniconda2/bin/python
+
+#simple script to make Relate map recombination file from LDhelmet output, but still needs some manual editing: 
+#e.g. for scaffold12 of Trichaptum:
+#cat output_Circumboreal_NorAm_NB_scaffold12_LDhat_lk_post_to_text.txt | grep -v "#\|a"  > output_Circumboreal_NorAm_NB_scaffold12_LDhat_lk_post_to_text_no_header.txt
+#get CM by multiplying second column with 100/(4*N_e) which is 100/(4*100000)= 0.00025
+#cat output_Circumboreal_NorAm_NB_scaffold12_LDhat_lk_post_to_text_no_header.txt | awk '{FS=" "}{print $1}' > testc1
+#cat output_Circumboreal_NorAm_NB_scaffold12_LDhat_lk_post_to_text_no_header.txt | awk '{FS=" "}{print $3}' > testc2
+#cat testc2 | awk '{print $1*0.00025}' > testc2_r
+#cat testc2_r | awk '{print $1/2.381207}' > testc2_r_mb #(length of scaffold12 is 2381207)
+#python make_recombination.py testc1_c2_r_mb testc1c2c3
+#edit in last line: 2346732 0	        7.6464006440790200
+#edit in headers: Position.bp. Rate.cM.Mb. Map.cM.
+#convert from tab to space delimitation
+
 import os
 import sys
 import io
